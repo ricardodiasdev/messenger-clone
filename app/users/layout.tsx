eb761abe84e4531bf8c2ea05e895e2007c96d7c9
Ricardo Dias <ricardodias.dev@gmail.com>
2023-05-15 18:56:25 -0300

    Implementando no layout UserList

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import getUsers from "../actions/getUsers";
 import SideBar from "../components/sidebar/SideBar";
+import UserList from "./components/UserList";
 
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers();
   return (
     //@ts-expect-error Server Component
     <SideBar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={users} /> 
+        {children}
+      </div>
     </SideBar>
   );
 }
