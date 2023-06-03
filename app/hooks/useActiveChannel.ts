09b03d7b3a943086ec1d221ba7b6982b630fb8a4
Ricardo Dias <ricardodias.dev@gmail.com>
2023-06-03 12:03:26 -0300

    Criando o hook useActiveChannel

---
+++ app/hooks/useActiveChannel.ts
@@ -0,0 +1,46 @@
+"use client";
+
+import { Channel, Members } from "pusher-js";
+import { useEffect, useState } from "react";
+import { pusherClient } from "../libs/pusher";
+import useActiveList from "./useActiveList";
+
+const useActiveChannel = () => {
+  const { set, add, remove } = useActiveList();
+  const [activeChannel, setActiveChannel] = useState<Channel | null>(null);
+
+  useEffect(() => {
+    let channel = activeChannel;
+
+    if (!channel) {
+      channel = pusherClient.subscribe("presence-messager");
+      setActiveChannel(channel);
+    }
+
+    channel.bind("pusher:subscription_succeeded", (members: Members) => {
+      const initialMembers: string[] = [];
+
+      members.each((member: Record<string, any>) =>
+        initialMembers.push(member.id)
+      );
+      set(initialMembers);
+    });
+
+    channel.bind("pusher:member_added", (member: Record<string, any>) => {
+        add(member.id)
+    })
+
+    channel.bind("pusher:member_removed", (member: Record<string, any>) => {
+        remove(member.id)
+    })
+
+    return () => {
+        if(activeChannel){
+            pusherClient.unsubscribe("presence-messager")
+            setActiveChannel(null)
+        }
+    }
+  }, [activeChannel, add, remove, set]);
+};
+
+export default useActiveChannel;
